c9832c026b98e8454e6dcf482d5c7bee68283b97
AbsoluteMorty <85866612+AbsoluteMorty@users.noreply.github.com>
2021-10-10 15:55:08 -0500

    add comments

--- queries.sql
+++ queries.sql
@@ -11,18 +11,22 @@ from employees
 where (birth_date between '1952-01-01' AND '1955-12-31')
 and (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+-- determine retirement eligibility
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
 
+-- determine retirement eligibility
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
 
+-- determine retirement eligibility
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
 
+-- determine retirement eligibility
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';

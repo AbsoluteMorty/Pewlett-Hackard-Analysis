a4d778527609fb2c5f1f83935be78c8d7045aab7
AbsoluteMorty <85866612+AbsoluteMorty@users.noreply.github.com>
2021-10-10 21:34:42 -0500

    add deliverable 2 to sql queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -31,3 +31,12 @@ order by count(title) desc
 
 -------------------------------------------------------------
 
+select distinct on(e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+into mentorship_eligibility
+from employees as e
+	inner join dept_emp as de
+		on e.emp_no = de.emp_no
+		inner join titles as t
+		on e.emp_no = t.emp_no
+where (de.to_date between '9999-01-01' and '9999-01-01') and (e.birth_date between '1965-01-01' AND '1965-12-31')
+order by e.emp_no
